9fd400249a010fb55367b535064cc058fdc63603
Filippoboe23 <fil.boeilboa@gmail.com>
2025-02-14 11:25:57 +0100

    Initial commit

---
+++ src/test/java/com/epicode/ViaggiAziendali/ViaggiAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.ViaggiAziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaggiAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
